5dbe29716607c0e6e220cee2c5c03eb3a394324c
Eduardo Blank Arrais Silva <101676501+EduBlankAS@users.noreply.github.com>
2023-08-24 21:53:33 -0300

    Add files via upload

---
+++ AplicationFixa/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using AplicationFixa.Models;
+
+namespace AplicationFixa.Data
+{
+    public class AppDbContext :
+        IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Psicologo>? psicologos{ get; set; }
+
+    }
+}
---
+++ AplicationFixa/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AplicationFixa.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
